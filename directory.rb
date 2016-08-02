06582f595890837ee33f2a69555319ecd6910ce3
supasuma <supasuma@gmail.com>
2016-08-02 09:58:53 +0100

    added method def short_names to filther out students whose name is shorter than 12 characters

--- directory.rb
+++ directory.rb
@@ -5,6 +5,7 @@ def input_students
   puts "To finish, just hit return twice"
 
   students = []
+
   name = gets.chomp
 
   while !name.empty? do
@@ -20,7 +21,19 @@ def print_header
   puts "-------------"
 end
 
-def print_students(students)
+students_with_short_names = []
+
+def short_names (students, students_with_short_names)
+  students.each do |student|
+    if student[:name].length < 12
+        students_with_short_names << student
+#push all names shorter than 12 to new array to be passed to print_students
+#if name is longer than 12 do nothing with the name
+    end
+  end
+end
+
+def print_students (students_with_short_names)
   puts "Would you like to filter students by the letter of their first name?"
   puts "If so, please enter the letter, otherwise hit enter"
   letter = gets.chomp
@@ -28,24 +41,25 @@ def print_students(students)
 #access name key and ask whether the string starts with letter puts only those names
 #PROBLEM - probably don't want print_footer when filtering, OR want a different footer that counts those with partic letter.
     if !letter.empty?
-      students.each_with_index do |student, i|
+      students_with_short_names.each_with_index do |student, i|
         if student[:name].start_with?(letter)
            puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)"
         end
       end
     else
-      students.each_with_index do |student, i|
+      students_with_short_names.each_with_index do |student, i|
       puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)"
       end
     end
-
 end
 #FInally we print the overall total of students
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students."
+  print "Overall, we have #{students.count} great students, but above are those "
+  puts "with names of less than 12 characters and/or those students filtered by the letter of their first name."
 end
 
 students = input_students
 print_header
-print_students(students)
+short_names(students, students_with_short_names)
+print_students(students_with_short_names)
 print_footer(students)
