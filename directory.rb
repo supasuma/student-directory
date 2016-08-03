1730edf5bf939caa88cc9b2db61cffa490e22f54
supasuma <supasuma@gmail.com>
2016-08-03 16:59:55 +0100

    providing the user the option to group output by cohort

--- directory.rb
+++ directory.rb
@@ -20,7 +20,7 @@ def input_students
 	date_correct = false
 
 	while !name.empty?
-    	students << {name: name, cohort: cohort}
+    	students << {name: name, cohort: cohort.to_sym}
     	puts "Now we have #{students.count} students".center(120)
     	puts "Please enter the name of the student".center(120)
     	name = gets.chomp.capitalize
@@ -46,52 +46,69 @@ def print_header
   puts "-------------".center(120)
 end
 
-students_with_short_names = []
+#students_with_short_names = []
 
-def short_names (students, students_with_short_names)
-  students.each do |student|
-    if student[:name].length < 12
-        students_with_short_names << student
+#def short_names (students, students_with_short_names)
+#  students.each do |student|
+#    if student[:name].length < 12
+#        students_with_short_names << student
 #push all names shorter than 12 to new array to be passed to print_students
 #if name is longer than 12 do nothing with the name
+#    end
+#  end
+#end
+
+def print_students (student_array)
+# puts "Would you like to filter students by the letter of their first name?".center(120)
+# puts "If so, please enter the letter, otherwise hit enter".center(120)
+# letter = gets.chomp.upcase
+# 	if !letter.empty?
+#  		filter_by_letter(student_array, letter)
+
+#  else
+puts "If you would you like to filter by cohort please type 'yes'".center(120)
+answer = gets.chomp.upcase
+if answer == "YES"
+
+  cohort_groups = []
+
+  cohort_groups << student_array.group_by { |student| student[:cohort] }
+
+  cohort_groups[0].each do |k,v|
+   	 puts k.to_s.capitalize.center(120)
+  		v.each_with_index do |x, i|
+  			puts (x[:name] + " (" + k.to_s.capitalize + " cohort)").center(120)
+  		end
+  end
+
+else
+    counter = 0
+    while student_array.length >= counter+1
+      puts "#{counter+1}. #{student_array[0+counter][:name]} (#{student_array[0+counter][:cohort]} cohort)".center(120)
+      counter+=1
     end
   end
 end
 
-def print_students (student_array)
- puts "Would you like to filter students by the letter of their first name?".center(120)
- puts "If so, please enter the letter, otherwise hit enter".center(120)
- letter = gets.chomp.upcase
- 	if !letter.empty?
-  		filter_by_letter(student_array, letter)
-  else
-		counter = 0
-		while student_array.length >= counter+1
-			puts "#{counter+1}. #{student_array[0+counter][:name]} (#{student_array[0+counter][:cohort]} cohort)".center(120)
-      puts
-			counter+=1
-		end
-	end
-end
+#def filter_by_letter(student_array, letter)
+#	counter = 0
+#	while student_array.length >= counter+1
+#		if student_array[counter][:name].start_with?(letter)
+#           puts "#{counter+1}. #{student_array[0+counter][:name]} (#{student_array[0+counter][:cohort]} cohort)".center(120)
+#           puts
+#        end
+#    counter+=1
+#    end
+#end
 
-def filter_by_letter(student_array, letter)
-	counter = 0
-	while student_array.length >= counter+1
-		if student_array[counter][:name].start_with?(letter)
-           puts "#{counter+1}. #{student_array[0+counter][:name]} (#{student_array[0+counter][:cohort]} cohort)".center(120)
-           puts
-        end
-    counter+=1
-    end
-end
 #FInally we print the overall total of students
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students, but above are those with names of ".center(120)
-  puts "less than 12 characters (and filtered by first name letter if you selected that option).".center(120)
+  puts "Overall, we have #{students.count} great students".center(120) #but above are those with names
+  #puts of less than 12 characters (and filtered by first name letter if you selected that option).".center(120)
 end
 
 students = input_students
 print_header
-short_names(students, students_with_short_names)
-print_students(students_with_short_names)
+#short_names(students, students_with_short_names)
+print_students(students)
 print_footer(students)
