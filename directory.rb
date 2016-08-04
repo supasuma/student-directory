d34e4cf82182842d9b6a70a4245f6c0138af2e22
supasuma <supasuma@gmail.com>
2016-08-04 15:37:34 +0100

    added def load_students, need to add code that will prevent the user from calling load_students more than once before exiting or else clear the @students variable.

--- directory.rb
+++ directory.rb
@@ -64,6 +64,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -76,6 +78,7 @@ def print_menu
   puts "1. Input students"
   puts "2. Show students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -162,6 +165,15 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
